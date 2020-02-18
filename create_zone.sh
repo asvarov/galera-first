eb1063aeae359492f5ea6d3fd21430e834e37e0b
root <root@hn01-bhs3.lab2.jelastic.team>
2020-02-18 15:32:48 +0000

    hzz

--- create_zone.sh
+++ create_zone.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-docker exec compose_pdns-server-1_1 pdnsutil create-zone lab2.jelastic.team
-docker exec compose_pdns-server-1_1 pdnsutil add-record lab2.jelastic.team www A 192.168.3.2
-docker exec compose_pdns-server-1_1 pdnsutil add-record lab2.jelastic.team ns A 192.168.3.2
-docker exec compose_pdns-server-1_1 pdnsutil add-record lab2.jelastic.team ns A 192.168.2.2
-docker exec compose_pdns-server-1_1 pdnsutil add-record lab2.jelastic.team hn01-bhs3 A 192.168.3.2
-docker exec compose_pdns-server-1_1 pdnsutil add-record lab2.jelastic.team hn01-waw1 A 192.168.2.2
+docker exec pdns-server pdnsutil create-zone lab2.jelastic.team
+docker exec pdns-server pdnsutil add-record lab2.jelastic.team www1 A 192.168.3.2
+docker exec pdns-server pdnsutil add-record lab2.jelastic.team www2 A 192.168.2.2
+docker exec pdns-server pdnsutil add-record lab2.jelastic.team ns1 A 192.168.3.2
+docker exec pdns-server pdnsutil add-record lab2.jelastic.team ns2 A 192.168.2.2
+docker exec pdns-server pdnsutil add-record lab2.jelastic.team hn01-bhs3 A 192.168.3.2
+docker exec pdns-server pdnsutil add-record lab2.jelastic.team hn01-waw1 A 192.168.2.2
+
+docker exec pdns-server pdnsutil delete-rrset lab2.jelastic.team www1 A
---
+++ git.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /opt/compose/webcontent
+git pull origin
